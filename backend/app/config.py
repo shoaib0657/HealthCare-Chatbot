e232413f2adce13efa490a4a8449d8e4f107a0a4
Mohammad Shoaib Ansari <mdshoaibansari0307@gmail.com>
2024-12-30 04:18:46 +0530

    add configuration settings

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,42 @@
 # Configuration settings (API keys, DB, etc.).
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config():
+    # General settings
+    APP_NAME = "HealthCare Chatbot"
+    APP_VERSION = "1.0"
+    DEBUG = os.getenv("DEBUG", False)
+
+    # API keys
+    GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+    GROQ_MODEL_NAME: str = os.getenv("GROQ_MODEL_NAME", "mixtral-8x7b-32768")
+    TEMPERATURE: float = float(os.getenv("TEMPERATURE", 0.7))
+    MAX_TOKENS: int = int(os.getenv("MAX_TOKENS", 2000))
+
+    # Vector Database settings
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+    PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME", "medical-chatbot")
+
+    # Authentication settings
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+    TOKEN_EXPIRATION_MINUTES = int(os.getenv("TOKEN_EXPIRATION_MINUTES", 30))
+
+    # File upload settings
+    UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "./uploads")
+    ALLOWED_EXTENSIONS = {'txt', 'pdf'}
+
+    # Logging Settings
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+    LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    LOG_FILE = "app.log"
+
+# Ensure upload folder exists
+if not os.path.exists(Config.UPLOAD_FOLDER):
+    os.makedirs(Config.UPLOAD_FOLDER)
+
+# Create an instance of Config to use throughout the app
+config = Config()
