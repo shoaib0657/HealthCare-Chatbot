a83608f31e62326f157bc7a979d03e0c443cd8fc
aditya-varma8019 <rvenkataditya@gmail.com>
2025-01-28 14:21:37 +0530

    till 15:30

--- frontend/src/components/pinecone/Pinecone.jsx
+++ frontend/src/components/pinecone/Pinecone.jsx
@@ -1,11 +1,18 @@
-import { Database } from "lucide-react";
+import { Database, LucideLoader2, MoveUp, RefreshCcw } from "lucide-react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Textarea } from "../ui/textarea";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { useState } from "react";
+import { Progress } from "../ui/progress";
 
 
 const Pinecone = () => {
-  return (
-        <div className="flex flex-col items-center p-24">
+    const [isUploading, setIsUploading] = useState(false);
+
+    return (
+        <main className="flex flex-col items-center p-24">
             <Card>
                 <CardHeader>
                     <CardTitle>
@@ -18,17 +25,43 @@ const Pinecone = () => {
                 <CardContent>
                     <div className="grid grid-cols-3 gap-4">
                         <div className="col-span-2 grid gap-4 border rounded-lg p-6">
-
+                            <div className="gap-4 relative">
+                                <Button className="absolute -right-4 -top-4" variant={"ghost"} size={"icon"}>
+                                    <RefreshCcw />
+                                </Button>
+                                <Label>Files List:</Label>
+                                <Textarea readOnly className="min-h-24 resize-none border p-3 shadow-none disabled:cursor-default focus-visible:ring-0 text-sm text-muted-foreground"
+                                />
+                            </div>
+                            <div className="grid grid-cols-2 gap-4">
+                                <div className="grid gap-2">
+                                    <Label>Index Name</Label>
+                                    <Input placeholder="index name" disabled={isUploading} className="disabled:cursor-default" />
+                                </div>
+                                <div className="grid gap-2">
+                                    <Label>Namespace</Label>
+                                    <Input placeholder="namespace" disabled={isUploading} className="disabled:cursor-default" />
+                                </div>
+                            </div>
                         </div>
-                        <Button variant="outline" className="w-full h-full">
-                            <Database size={50} />
+                        <Button variant={"outline"} className="w-full h-full" disabled={isUploading} >
+                            <span className="flex flex-row">
+                                <Database size={50} className="stroke-[#D90013]" />
+                                <MoveUp className="stroke-[#D90013]" />
+                            </span>
                         </Button>
                     </div>
+                    {isUploading && <div className="mt-4">
+                        <Label>File Name:</Label>
+                        <div className="flex flex-row items-center gap-4">
+                            <Progress value={80} />
+                            <LucideLoader2 className="stroke-[#D90013] animate-spin" />
+                        </div>
+                    </div>}
                 </CardContent>
             </Card>
-        </div>
-  )
+        </main>
+    )
 };
 
 export default Pinecone;
-    
